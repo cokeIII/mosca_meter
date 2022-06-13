7389a0b1954173c3ace9bfaf68df6a3bd0045625
cokeIII <coke2484@gmail.com>
2022-06-13 21:24:50 +0700

    f

---
+++ index.js
@@ -0,0 +1,20 @@
+var mosca = require('mosca');
+var setting = {
+    port: 1883,
+    http: {
+        port: 8883
+    }
+}
+var server = new mosca.Server(setting)
+server.on('ready', setup)
+
+function setup() {
+    server.authenticate = authenticate
+    console.log("mosca server is up and runing (auth)")
+}
+
+var authenticate = function(client, username, password, callback) {
+    var authorized = (username === 'mqtt' && password.toString() === 'password')
+    if (authorized) client.user = username
+    callback(null, authorized)
+}
