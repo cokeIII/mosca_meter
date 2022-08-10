c15ae9d85fa52a15300e4c5b8e746d015f0780e1
cokeIII <coke2484@gmail.com>
2022-08-10 08:39:17 +0700

    f

--- index.js
+++ index.js
@@ -1,4 +1,46 @@
 var mosca = require('mosca');
+var mysql = require('mysql');
+var express = require("express");
+var bodyParser = require('body-parser');
+
+
+var db_config = {
+    host: 'localhost',
+    user: 'root',
+    password: 'chontech2021!',
+    database: 'ctc_meter'
+};
+
+var connection;
+
+function handleDisconnect() {
+    connection = mysql.createConnection(db_config); // Recreate the connection, since
+    // the old one cannot be reused.
+
+    connection.connect(function(err) { // The server is either down
+        if (err) { // or restarting (takes a while sometimes).
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+        } // to avoid a hot loop, and to allow our node script to
+    }); // process asynchronous requests in the meantime.
+    // If you're also serving http, display a 503 error.
+    connection.on('error', function(err) {
+        console.log('db error', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+            handleDisconnect(); // lost due to either server restart, or a
+        } else { // connnection idle timeout (the wait_timeout
+            throw err; // server variable configures this)
+        }
+    });
+}
+
+handleDisconnect();
+
+var app = express();
+
+var http = require('http').Server(app);
+app.use(bodyParser.json());
+
 require('dotenv').config();
 var settings = {
     port: 1883,
@@ -6,14 +48,17 @@ var settings = {
         port: 8883
     }
 };
-var server = new mosca.Server(settings); //สร้างตัวแปรมารับค่า server
-server.on('ready', setup); //ใช้คำสั่ง ready,setup เพื่อตั้งค่า
+app.get("/test", function(req, res) {
+    console.log("READY")
+});
+var server = new mosca.Server(settings); //à¸ªà¸£à¹‰à¸²à¸‡à¸•à¸±à¸§à¹à¸›à¸£à¸¡à¸²à¸£à¸±à¸šà¸„à¹ˆà¸² server
+server.on('ready', setup); //à¹ƒà¸Šà¹‰à¸„à¸³à¸ªà¸±à¹ˆà¸‡ ready,setup à¹€à¸žà¸·à¹ˆà¸­à¸•à¸±à¹‰à¸‡à¸„à¹ˆà¸²
 function setup() {
-    // server.authenticate = authenticate; // ตั้งให้เซิพเวอร์ต้องมี Authen
+    // server.authenticate = authenticate; // à¸•à¸±à¹‰à¸‡à¹ƒà¸«à¹‰à¹€à¸‹à¸´à¸žà¹€à¸§à¸­à¸£à¹Œà¸•à¹‰à¸­à¸‡à¸¡à¸µ Authen
     console.log('Mosca server is up and running (auth)')
 }
 var authenticate = function(client, username, password, callback) {
-    var authorized = (username === process.env.MQTT_USERNAME && password.toString() === process.env.MQTT_PASSWORD);
+    var authorized = (username === "ctc" && password.toString() === "12345678");
     if (authorized) client.user = username;
     callback(null, authorized);
 }
@@ -24,6 +69,27 @@ server.on('clientDisconnected', function(client) {
     console.log('Client Disconnected:', client.id);
 });
 server.on('published', function(packet, client) {
-    console.log(packet);
-    console.log('Published', packet.payload.toString());
+    //console.log(packet);
+    //console.log('Published', packet.payload.toString());
+    const myArray = packet.payload.toString().substr(1, packet.payload.toString().length - 2).split(",");
+    //console.log(myArray)
+    if (packet.topic == "data_meter") {}
 });
+
+function insertData(myArray) {
+    let sql = 'insert into log_data (id,v1,v2,v3,c1,c2,c3,kw,kwh,hz,pf1,pf2,pf3) values("' + myArray[0] + '","' + myArray[1] + '","' + myArray[2] + '","' + myArray[3] + '","' + myArray[4] + '","' + myArray[5] + '","' + myArray[6] + '","' + myArray[7] + '","' + myArray[8] + '","' + myArray[9] + '","' + myArray[10] + '","' + myArray[11] + '","' + myArray[12] + '")'
+    connection.query(sql, function(err, result) {
+        if (!err) {
+            if (result.affectedRows) {
+                console.log("insert OK")
+            }
+        } else {
+            console.log(sql)
+        }
+    });
+}
+connection.on('error', function(err) {
+    console.log("[mysql error]", err);
+});
+
+app.listen(7777);
